1a1bcbc662d380c06b62c32cff77b95764fccd66
Rishi <rishibansal02@gmail.com>
2021-09-20 19:08:28 +0530

    .

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+        System.out.println("Hello World Primary!");
+        int stuAge = 5;
+        double stuGPA = 8.28;
+        System.out.println(stuAge);
+        System.out.println(stuGPA);
     }
 }
